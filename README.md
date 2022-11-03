436541f63b2495166361e2cc056ecd61a170c5df
Keegan Campbell <me@kfcampbell.com>
2022-11-03 13:20:33 -0700

    Make script generic

--- README.md
+++ README.md
@@ -2,13 +2,23 @@
 
 > Add files and create PRs across repositories. Useful for generating issue and PR templates.
 
+The tool will scan the `templates` directory to determine issue templates. It will create an issue template for each file there, branch off of the latest default branch commit, and create a PR with the issue templates added.
+
+To PR issue templates to `owner/repoName`, call:
+
+```bash
+node ./cli.js -T ${GITHUB_TOKEN} -R owner/repoName
+```
+
+Note that you must give the octoherd CLI permission for requests (granted interactively in the script).
+
 ## Contributing
 
 See [CONTRIBUTING.md](CONTRIBUTING.md)
 
 ## About Octoherd
 
-[@octoherd](https://github.com/octoherd/) is project to help you keep your GitHub repositories in line.
+[@octoherd](https://github.com/octoherd/) is a project to help you keep your GitHub repositories in line.
 
 ## License
 
--- script.js
+++ script.js
@@ -1,232 +1,7 @@
 // @ts-check
-const bugTemplate = {
-  name: "bug",
-  content: `name: Bug Report
-description: File a bug report
-title: "[Bug]: "
-labels: ["bug", "triage"]
-assignees:
-  - octocat
-body:
-  - type: markdown
-    attributes:
-      value: |
-        Thanks for taking the time to fill out this bug report!
-  - type: input
-    id: contact
-    attributes:
-      label: Contact Details
-      description: How can we get in touch with you if we need more info?
-      placeholder: ex. email@example.com
-    validations:
-      required: false
-  - type: textarea
-    id: what-happened
-    attributes:
-      label: What happened?
-      description: Also tell us, what did you expect to happen?
-      placeholder: Tell us what you see!
-      value: "A bug happened!"
-    validations:
-      required: true
-  - type: dropdown
-    id: version
-    attributes:
-      label: Version
-      description: What version of our software are you running?
-      options:
-        - 1.0.2 (Default)
-        - 1.0.3 (Edge)
-    validations:
-      required: true
-  - type: dropdown
-    id: browsers
-    attributes:
-      label: What browsers are you seeing the problem on?
-      multiple: true
-      options:
-        - Firefox
-        - Chrome
-        - Safari
-        - Microsoft Edge
-  - type: textarea
-    id: logs
-    attributes:
-      label: Relevant log output
-      description: Please copy and paste any relevant log output. This will be automatically formatted into code, so no need for backticks.
-      render: shell
-  - type: checkboxes
-    id: terms
-    attributes:
-      label: Code of Conduct
-      description: By submitting this issue, you agree to follow our [Code of Conduct](https://example.com)
-      options:
-        - label: I agree to follow this project's Code of Conduct
-          required: true
-`}
 
-const docTemplate = {
-	  name: "doc",
-	  content: `name: Documentation
-description: Update, add, or improve documentation
-title: "[DOCS]: "
-labels: ["Type: Documentation", "Status: Triage"]
-assignees:
-  -
-body:
-  - type: markdown
-    attributes:
-      value: |
-        Thanks for taking the time to fill this out!
-  - type: textarea
-    id: describe-need
-    attributes:
-      label: Describe the need
-      description: Also tell us, whats expected?
-      placeholder: Why are these updates needed!
-      value: "I need the docs!"
-    validations:
-      required: true
-  - type: input
-    id: sdk_version
-    attributes:
-      label: SDK Version
-      description: Do these docs apply to a specific SDK version?
-      placeholder: ex. v1.1.1
-    validations:
-      required: false
-  - type: input
-    id: api_version
-    attributes:
-      label: REST API Version
-      description: Do these docs apply to a specific version of the GitHub REST API?
-      placeholder: ex. v1.1.1
-    validations:
-      required: false
-  - type: textarea
-    id: logs
-    attributes:
-      label: Relevant log output
-      description: Please copy and paste any relevant source code or log details. This will be automatically formatted into code, so no need for backticks.
-      render: shell
-  - type: checkboxes
-    id: terms
-    attributes:
-      label: Code of Conduct
-      description: By submitting this issue, you agree to follow our [Code of Conduct](https://example.com)
-      options:
-        - label: I agree to follow this project's Code of Conduct
-          required: true`}
-
-const featureTemplate = {
-	name: "feature",
-	content: `name: Feature
-description: Suggest an idea for a new feature or enhancement
-title: "[FEAT]: "
-labels: ["Type: Feature", "Status: Triage"]
-assignees:
-  -
-body:
-  - type: markdown
-    attributes:
-      value: |
-        Thanks for taking the time to fill this out!
-  - type: textarea
-    id: describe-need
-    attributes:
-      label: Describe the need
-      description: Also tell us, whats expected/wanted?
-      placeholder: Why is this feature needed!
-      value: "I really need this feature!"
-    validations:
-      required: true
-  - type: input
-    id: sdk_version
-    attributes:
-      label: SDK Version
-      description: Does this feature suggestion apply to a specific SDK version?
-      placeholder: ex. v1.1.1
-    validations:
-      required: false
-  - type: input
-    id: api_version
-    attributes:
-      label: REST API Version
-      description: Does this feature suggestion apply to a specific version of the GitHub REST API?
-      placeholder: ex. v1.1.1
-    validations:
-      required: false
-  - type: textarea
-    id: logs
-    attributes:
-      label: Relevant log output
-      description: Please copy and paste any relevant source code or log details. This will be automatically formatted into code, so no need for backticks.
-      render: shell
-  - type: checkboxes
-    id: terms
-    attributes:
-      label: Code of Conduct
-      description: By submitting this issue, you agree to follow our [Code of Conduct](https://example.com)
-      options:
-        - label: I agree to follow this project's Code of Conduct
-          required: true`
-}
-
-const maintenanceTemplate = {
-	name: "maintenance",
-	content: `name: Maintenance
-description: Dependencies, Cleanup, refactoring, reworking of code
-title: "[MAINT]: "
-labels: ["Type: Maintenance", "Status: Triage"]
-assignees:
-  -
-body:
-  - type: markdown
-    attributes:
-      value: |
-        Thanks for taking the time to fill this out!
-  - type: textarea
-    id: describe-need
-    attributes:
-      label: Describe the need
-      description: Also tell us, whats expected/wanted?
-      placeholder: Why is this maintenance needed!
-      value: "The code needs some serious help here!"
-    validations:
-      required: true
-  - type: input
-    id: sdk_version
-    attributes:
-      label: SDK Version
-      description: Does this maintenance apply to a specific SDK version?
-      placeholder: ex. v1.1.1
-    validations:
-      required: false
-  - type: input
-    id: api_version
-    attributes:
-      label: REST API Version
-      description: Does this maintenance apply to a specific version of the GitHub REST API?
-      placeholder: ex. v1.1.1
-    validations:
-      required: false
-  - type: textarea
-    id: logs
-    attributes:
-      label: Relevant log output
-      description: Please copy and paste any relevant source code or log details. This will be automatically formatted into code, so no need for backticks.
-      render: shell
-  - type: checkboxes
-    id: terms
-    attributes:
-      label: Code of Conduct
-      description: By submitting this issue, you agree to follow our [Code of Conduct](https://example.com)
-      options:
-        - label: I agree to follow this project's Code of Conduct
-          required: true`
-}
-
-const templates = [bugTemplate, docTemplate, featureTemplate, maintenanceTemplate]
+import * as fs from 'fs';
+import path from 'path';
 
 /**
  * generates PRs for octokit  repos
@@ -236,6 +11,24 @@ const templates = [bugTemplate, docTemplate, featureTemplate, maintenanceTemplat
  */
 export async function script(octokit, repository, options) {
 
+  // get list of all files in templates directory
+  const files = fs.readdirSync('./templates');
+
+  // iterate through files and store the string content of each file
+  const templates = await Promise.all(
+    files.map(async (file) => {
+      // read the string content of each file in the templates directory
+      const template = await fs.promises.readFile(
+        path.join('./templates', file),
+        'utf8'
+      );
+      return {
+        name: file,
+        content: template,
+      };
+    })
+  );
+
 	// get SHA of latest default branch commit
 	const { data: { object: { sha } } } = await octokit.request("GET /repos/{owner}/{repo}/git/ref/{ref}", {
 		owner: repository.owner.login,
@@ -243,7 +36,7 @@ export async function script(octokit, repository, options) {
 		ref: `heads/${repository.default_branch}`,
 	});
 
-	// create a branch called "octoherd-script-PR"
+	// create a branch called "octoherd-script-PR" off of the latest SHA
 	const branch = await octokit.request("POST /repos/{owner}/{repo}/git/refs", {
 		owner: repository.owner.login,
 		repo: repository.name,
